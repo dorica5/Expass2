a8d3fb91b1ab66f37ba8e8e086ee532aefc41b02
Dorcas <dorcasmihio@gmail.com>
2024-09-04 11:03:35 +0200

    Initial commit

---
+++ src/main/java/hvl/dat250/Expass2/Expass2Application.java
@@ -0,0 +1,13 @@
+package hvl.dat250.Expass2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Expass2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Expass2Application.class, args);
+	}
+
+}
---
+++ src/test/java/hvl/dat250/Expass2/Expass2ApplicationTests.java
@@ -0,0 +1,13 @@
+package hvl.dat250.Expass2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Expass2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
